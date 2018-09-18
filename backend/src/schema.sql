03c8114699e8b2d654b5d1a25e0041118a05400e
Magnus <magnull@stud.ntnu.no>
2018-09-18 09:48:13 +0200

    changes to the sql schema

--- backend/src/schema.sql
+++ backend/src/schema.sql
@@ -4,10 +4,11 @@ USE `onthelinedb`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(250) NOT NULL,
-  `groupid` int(5) NOT NULL
+  `salt` varchar(20) NOT NULL,
+  `gid` int(5) NOT NULL
 );
 
 DROP TABLE IF EXISTS `group`;
@@ -18,27 +19,23 @@ CREATE TABLE `group` (
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
+  `displayname` varchar(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS `thread`;
 CREATE TABLE `thread` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `threadname` varchar(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(30) NOT NULL,
   `content` varchar(10000) NOT NULL,
   `timestamp` varchar(50) NOT NULL
 );
-
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD CONSTRAINT `group_id_ref_group` FOREIGN KEY (`groupid`) REFERENCES
-  `groupid` (`group`) ON UPDATE CASCADE;
 
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
@@ -46,5 +43,35 @@ ALTER TABLE `category`
 ALTER TABLE `thread`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`groupid`);
+
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD CONSTRAINT `group_refers_id` FOREIGN KEY (`gid`) REFERENCES
+  `groupid` (`group`) ON UPDATE CASCADE;
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `thread`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `group`
+  MODIFY `groupid` int(11) NOT NULL AUTO_INCREMENT;
+
+
+INSERT INTO group (groupid, groupname)
+  VALUES (1000, "admin"), (6969, "user");
+
+INSERT INTO user (username, groupid)
+  VALUES ("AdminUser", 1000), ("Normaluser", 6969);

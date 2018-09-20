90ebf6153f880800fcc679439cf2fac6c27e8fa2
Joakim Ellestad <joakine@stud.ntnu.no>
2018-09-20 13:36:49 +0200

    Fixed an issue on init_db where the sql schema could not be read by the db.

--- backend/src/db.py
+++ backend/src/db.py
@@ -6,13 +6,13 @@ from flask.cli import with_appcontext
 
 DB_NAME='onthelinedb'
 # Reference for mysql: https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
-# Get a couchdb session
 # Returns the already established connection if it exists or tries to
 # create a new one.
 # Handles error for "access denied" and "unknown database"
 def get_db():
     if 'db' not in g:
         try:
+            click.echo("Trying to connect")
             cnx = mysql.connector.connect(user='tom', password='jerry',
                               host='127.0.0.1',
                               database=DB_NAME)
@@ -30,24 +30,26 @@ def get_db():
         # Don't bother continueing execution if connection to db cannot
         # be established. DB is life.
         exit(1)
+    click.echo("Using existing connection")
     return g.db
-
-# Closes session
-def close_db(e=None):
-    cnx = g.pop('db', None)
-
-    if cnx is not None:
-        cnx.close()
 
 def init_db():
     cnx = get_db()
     cursor = cnx.cursor()
     with current_app.open_resource('schema.sql') as f:
         try:
-            cursor.execute(f)
+            cursor.execute(f.read().decode('utf8'))
         except mysql.connector.Error as err:
             click.echo("Failed creating database: {}".format(err))
             exit(1)
+
+
+# Closes session
+def close_db(e=None):
+    click.echo("close_db")
+    cnx = g.pop('db', None)
+    if cnx is not None:
+        cnx.close()
 
 
 @click.command('init-db')

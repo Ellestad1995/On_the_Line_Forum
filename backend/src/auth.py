332bc8127a0c0fb8d02d289df434841c8514a194
Joakim Ellestad <joakine@stud.ntnu.no>
2018-09-20 17:00:13 +0200

    litt testing med å generere grafisk

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -6,23 +6,47 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Obviously we need database access in functions here
-# the '.' represents __init__.py / this project. And we important the get_db function.
+# the '.' represents this repo / this project. And we important the get_db function.
 from .db import get_db
 
 # The blueprint /auth will represent the endpoints starting with /auth
 # See the endpoint csv file for what is included.
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    authorized = session.get('auth_cookie')
+    if authorized is None:
+        g.user = None
+    else:
+        cnx = get_db()
+        cursor = cnx.cursor()
+        g.user = cursor.execute('SELECT * FROM users WHERE id = ? ', ())
+
+
+
+
 # /auth
 @bp.route('/', methods=['GET'])
-def loginCheck():
+def login():
     """
         Registers a new user. Provides a form so the
         user can enter email, passord.
     """
     if request.method == 'GET':
         db = get_db()
-        click.echo("Seems like we could do it this way")
+        return render_template('base.html')
+
+@bp.route('/profile', methods=['GET'])
+def profile():
+    """
+    Display a users information
+    """
+    if request.method == 'GET':
+        return render_template('base.html')
 
 # /auth/user POST
 # Post request for creating a new user

564229d744cc6bbf9fd57180bdd6daf2559008c1
Magnus <magnull@stud.ntnu.no>
2018-09-24 19:24:43 +0200

    working db schema

--- backend/src/schema.sql
+++ backend/src/schema.sql
@@ -48,7 +48,9 @@ CREATE TABLE `post` (
   FOREIGN KEY (`threadid`) REFERENCES `thread` (`id`) ON DELETE CASCADE,
   PRIMARY KEY (`id`)
 );
-INSERT INTO `thread` (`threadname`, `categoryid`) VALUES ("Trump", "1"), ("Obama", "1"), ("Red-team", "2"), ("Blue team", "2");
+
 INSERT INTO `category` (`displayname`) VALUES ("Politics"), ("Haking"), ("Cars"), ("Raid");
+INSERT INTO `thread` (`threadname`, `categoryid`) VALUES ("Trump", "1"), ("Obama", "1"), ("Red-team", "2"), ("Blue team", "2");
 INSERT INTO `group` VALUES (1000, "admin"), (6969, "user");
-INSERT INTO `user` (`username`, `password`, `salt`, `groupid`) VALUES ("AdminUser", "tmppassword", "2FGJM6GSDS", 1000);
+INSERT INTO `user` (`username`, `password`, `groupid`, `token`) VALUES ("AdminUser", "tmppassword", "1000", "6969testtoken6969");
+INSERT INTO `user` (`username`, `password`, `groupid`, `token`) VALUES ("Ola Nordman", "easypass", "6969", "6969testtoken6969");

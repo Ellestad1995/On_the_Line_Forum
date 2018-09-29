578ddef38768ef15d9d0419bb4ce96672d41fa3b
Joakim Ellestad <joakine@stud.ntnu.no>
2018-09-29 15:12:53 +0200

    userclass takes a row and stores a user inMemory

--- backend/src/objects/UserClass.py
+++ backend/src/objects/UserClass.py
@@ -1,11 +1,6 @@
-class User:
-    id = None
-    username = None
-    email = None
-    groupId = None
-    sessionTime = None
-
-
+import click
+class User():
+    username = "starlord"
     def __init__(self):
         """
         Creates empty User object. Probably not very useful
@@ -19,13 +14,14 @@ class User:
         This can handle all the data defined there, but might not use it all.
         """
         error = None
-
-        if error is not None:
+        #click.echo("Info recieved: {Id}, {Username}".format(Id=row["id"],Username=row["username"]))
+        if error is None:
             # Assumes one row recieved
             # Assumes we have actually gotten a user row.
             if row["id"] is not None:
                 self.id = row["id"]
             if row["username"] is not None:
+                click.echo("Handling username: {}".format(row["username"]))
                 self.username = row["username"]
             if row["email"] is not None:
                 self.email = row["email"]
@@ -33,5 +29,19 @@ class User:
                 self.groupId = row["groupid"]
             if row["tokentimestamp"] is not None:
                 self.sessionTime = row["tokentimestamp"]
+            else:
+                self.sessionTime = None
 
-        
+    def dumpInfo(self):
+        """
+        Used mostly for debugging purpose.
+        It just dumpts whatever info it has with click.echo
+        NB: Never dump the token. Think like a script kiddie.
+        """
+        click.echo("""
+        id: {I}
+        username: {U}
+        email: {E}
+        groupId: {G}
+        session: {S}
+        """.format(I=self.id, U=self.username, E=self.email, G=self.groupId, S=self.sessionTime))

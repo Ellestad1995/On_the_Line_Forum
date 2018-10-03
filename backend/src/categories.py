ecdcad92da4278103216b2bd94615f702f966127
Joakim Ellestad <joakine@stud.ntnu.no>
2018-10-03 12:20:17 +0200

    Categories reads session object

--- backend/src/categories.py
+++ backend/src/categories.py
@@ -8,12 +8,38 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Obviously we need database access in functions here
 # the '.' represents __init__.py / this project. And we important the get_db function.
 from .db import get_db
-
+from .objects.UserClass import User
 bp = Blueprint('categories', __name__, url_prefix='/categories/')
 
-@bp.route('', methods=['GET'])
+@bp.before_app_request
+def load_logged_in_user():
+    """
+    If a user id is stored in the session, load the user object from
+    the database into ``g.user``.
+    """
+
+    authorized = session.get('access_token')
+    click.echo("What is wrong: {}".format(authorized))
+    if authorized is None:
+        g.user = None
+    else:
+        cnx = get_db()
+        cursor = cnx.cursor(dictionary=True)
+        query = 'SELECT id, username, groupid, email, tokentimestamp FROM user WHERE token = %s '
+        cursor.execute(query, (authorized,))
+        row = cursor.fetchone()
+        if row is not None:
+            g.user = User(row)
+            #g.user.dump()
+            click.echo("Categories: {}".format(g.user.username))
+        else:
+            click.echo("No user was found. Return to homepage")
+
+
+
+
+@bp.route('/', methods=['GET'])
 def index():
-    click.echo(str(g.user))
     cnx = get_db()
     cursor = cnx.cursor()
     cursor.execute("select id, displayname FROM category")
@@ -22,13 +48,14 @@ def index():
     adminuser = True
     return render_template('category/index.html', categories=categories, adminuser=adminuser)
 
+
 @bp.route('', methods=['POST'])
 def add_entry():
-    cnx = get_db()    
+    cnx = get_db()
     cursor = cnx.cursor()
     newcategory = request.form["new"]
     deletecategory = request.form["delete"]
-   
+
     if newcategory:
         if len(newcategory) < 30:
             cursor.execute("SELECT displayname FROM category where displayname = %s", (newcategory,))
@@ -49,6 +76,5 @@ def add_entry():
             cnx.commit()
         else:
             flash("What want you delete does not exsist")
-        
-    return redirect("/")
 
+    return redirect("/")
